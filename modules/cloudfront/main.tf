94218210287605670607dddce7d10f36ada3f94a
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-10-08 14:21:55 -0300

    policies

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -1,6 +1,6 @@
 locals {
-  s3_origin_oai = "bucket-oai"
   s3_origin_oac = "bucket-oac"
+  s3_origin_oai = "bucket-oai"
 }
 
 resource "aws_cloudfront_origin_access_identity" "main" {
@@ -21,48 +21,54 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   is_ipv6_enabled = true
   comment         = "Distribution for OAI and OAC bucket origins"
 
+  # OAC
+  origin {
+    domain_name = var.oac_bucket_regional_domain_name
+    origin_id   = local.s3_origin_oac
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.main.id
+  }
+
   # OAI
   origin {
     domain_name = var.oai_bucket_regional_domain_name
     origin_id   = local.s3_origin_oai
-    origin_path = "/oai"
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.main.cloudfront_access_identity_path
     }
   }
 
-  # OAC
-  origin {
-    domain_name = var.oac_bucket_regional_domain_name
-    origin_id   = local.s3_origin_oac
-    origin_path = "/oac"
-
-    origin_access_control_id = aws_cloudfront_origin_access_control.main.id
-  }
-
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["HEAD", "GET"]
-    target_origin_id = local.s3_origin_oai
+  ordered_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["HEAD", "GET"]
+    target_origin_id       = local.s3_origin_oac
+    path_pattern           = "/oac/*"
+    viewer_protocol_policy = "redirect-to-https"
 
     # CachingDisabled managed policy
     cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
-
-    viewer_protocol_policy = "allow-all"
   }
 
   ordered_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["HEAD", "GET"]
-    target_origin_id = local.s3_origin_oac
-
-    path_pattern = "/oac"
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["HEAD", "GET"]
+    target_origin_id       = local.s3_origin_oai
+    path_pattern           = "/oai/*"
+    viewer_protocol_policy = "redirect-to-https"
 
     # CachingDisabled managed policy
     cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+  }
 
-    viewer_protocol_policy = "allow-all"
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.s3_origin_oac
+    viewer_protocol_policy = "redirect-to-https"
+
+    # CachingOptimized
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
   }
 
   restrictions {
